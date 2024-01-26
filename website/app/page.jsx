2e73bf1bf5e74aab10a36c64fbb114f1373cb282
Simon Quach <simonquach.tech@gmail.com>
2024-01-26 00:38:59 -0800

    BEAC-1 frontend

---
+++ website/app/page.jsx
@@ -0,0 +1,20 @@
+import Nav from "../components/Nav";
+import Hero from "../components/Hero";
+import Problem from "../components/Problem";
+import Solution from "../components/Solution";
+import Features from "../components/Features";
+
+export default function Home() {
+  return (
+    <main className="tracking-wide w-full flex flex-col items-center">
+      <div className="relative md:block hidden">
+        <Nav />
+      </div>
+
+      <Hero />
+      <Problem />
+      <Solution />
+      <Features />
+    </main>
+  );
+}
---
+++ website/components/Solution.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+import Glasses from "../assets/images/glasses.png";
+
+const Solution = () => {
+  return (
+    <div className="text-white flex flex-col lg:flex-row justify-between items-center lg:space-x-[6rem] space-y-[6rem] lg:space-y-0 w-full py-[16rem] md:px-[4rem] px-[1rem] max-w-[1200px] mt-[1rem]">
+      <div>
+        <div className="inline font-bold bg-gradient-to-r from-[#6497E3] via-[#976FEB] to-[#E86FC6] text-transparent bg-clip-text">
+          SOLUTION
+        </div>
+        <div className="text-[32px] font-bold">Introducing Beacon</div>
+        <div>
+          Our glasses redefine wearable technology by seamlessly merging the
+          digital with the physical. Equipped with essential components like a
+          camera, microphone, and speaker, and complemented by a user-friendly
+          Webapp, they offer an intuitive, immersive experience, bridging the
+          gap between what you know and what you're yet to discover.
+        </div>
+      </div>
+      <Image src={Glasses} />
+    </div>
+  );
+};
+
+export default Solution;
