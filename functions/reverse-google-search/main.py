f11c6cffa87c7e4de813058bd669d774f3d87de0
Simon Quach <simonquach04@gmail.com>
2024-02-22 18:41:46 -0800

    google lens

--- functions/reverse-google-search/main.py
+++ functions/reverse-google-search/main.py
@@ -1,14 +1,9 @@
-import requests
 from google.cloud import storage
-from dotenv import load_dotenv
-import datetime
 import os
-from langchain.pydantic_v1 import BaseModel, Field
-from langchain.tools import BaseTool, StructuredTool, tool
 import uuid
+import serpapi
+from openai import OpenAI
 import base64
-
-load_dotenv()
 
 
 def upload_blob(bucket_name, base64_data, destination_blob_name):
@@ -26,49 +21,51 @@ def upload_blob(bucket_name, base64_data, destination_blob_name):
     print(f"Data uploaded to {destination_blob_name}.")
 
 
-def generate_signed_url(bucket_name, blob_name):
-    """Generates a signed URL for a blob."""
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(blob_name)
-
-    # This URL is valid for 15 minutes
-    url = blob.generate_signed_url(
-        expiration=datetime.timedelta(minutes=15), version="v4"
+def identify_object(lst):
+    client = OpenAI()
+    system_prompt = "Your task is to identify the most common and specific subject in a given list of image titles, sorted in non-decreasing order of priority. Return the result in the format {title: <title>, object: <object>}, where <title> is the most specific name of the subject directly from the list, and <object> is the general category or type of the item. Prioritize earlier items in the list for determining the most common subject. Ensure specificity in identification, applicable to various subjects like technology, food, or other categories. For example, if the most common subject is 'Nikon D850', the response should be {title: 'Nikon D850', object: 'camera'}."
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": system_prompt,
+            },
+            {
+                "role": "user",
+                "content": f"List: {lst}",
+            },
+        ],
     )
 
-    return url
+    return completion.choices[0].message.content
 
 
-def reverse_image_search_api(image_url):
-    print(image_url)
-    api_key = os.getenv("SERPAPI_API_KEY")
-
-    search_url = f"https://serpapi.com/search?engine=google_reverse_image&api_key={api_key}&image_url={image_url}"
-
-    response = requests.get(search_url)
-    data = response.json()
-
-    return data
-
-
-@tool("reverse-image-search")
-def reverse_image_search(base64_image: str) -> dict:
+def google_lens_search(base64_image: str):
     """Reverse Google image search."""
+    api_key = os.getenv("SERPAPI_API_KEY")
     file_id = str(uuid.uuid4())
-    bucket_name = "beacon-reverse-image-api"
+    bucket_name = "treehacks24-vr"
     upload_blob(bucket_name, base64_image, file_id)
 
     image_url = f"https://storage.googleapis.com/{bucket_name}/{file_id}"
-    search_results = reverse_image_search_api(image_url)
 
-    return search_results
+    params = {
+        "engine": "google_lens",
+        "url": image_url,
+        "api_key": os.getenv("SERPAPI_API_KEY"),
+    }
+
+    search = serpapi.search(params)
+    visual_matches = search["visual_matches"]
+    titles = [match["title"] for match in visual_matches if "title" in match]
+    res = identify_object(titles)
+    return res
 
 
-# image_path = "./car.png"
-# with open(image_path, "rb") as image_file:
-#     image_data = image_file.read()
-# image_base64 = base64.b64encode(image_data)
-# image_base64_str = image_base64.decode("utf-8")
-
-# print(reverse_image_search(image_base64_str))
+def convert_to_base64(image_path: str):
+    with open(image_path, "rb") as image_file:
+        image_data = image_file.read()
+    image_base64 = base64.b64encode(image_data)
+    image_base64_str = image_base64.decode("utf-8")
+    return image_base64_str
