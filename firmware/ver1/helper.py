965aee83f7aa6884bcf5c0eeb8d613f7c87b7b5b
Bill Zhang <billzhangsc@gmail.com>
2024-03-03 03:23:22 -0800

    Firmware v1

---
+++ firmware/ver1/helper.py
@@ -0,0 +1,136 @@
+import numpy as np
+import shutil
+import base64
+import pyaudio
+import collections
+import websockets
+import asyncio
+import json
+import cv2
+import queue
+import threading
+import io
+import wave
+# from picamera2 import Picamera2
+
+# mpv
+import shutil
+import subprocess
+
+def is_installed(lib_name):
+    lib = shutil.which(lib_name)
+    if lib is None:
+        return False
+    return True 
+
+def play_audio_stream(audio_queue):
+    """
+    Function to be run in a separate thread for playing audio chunks.
+    It takes a queue from which it will continuously read and play audio chunks.
+    """
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16,  # 16-bit PCM format
+                    channels=1,  # mono audio
+                    rate=44100,  # Adjusted to 44.1kHz sample rate for pcm_44100
+                    output=True)
+
+    try:
+        while True:
+            chunk = audio_queue.get()  # Block until an item is available
+            if chunk is None:
+                break  # None is the signal to stop
+            data = base64.b64decode(chunk)
+            stream.write(data)
+    finally:
+        # Ensure resources are released even if there's an error
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        
+def capture_image_to_base64_opencv():
+    # Initialize the default camera
+    cap = cv2.VideoCapture(0)  # 0 is the default camera
+
+    # Check if the webcam is opened correctly
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
+    
+    # Capture an image from the camera
+    ret, frame = cap.read()
+    if not ret:
+        raise IOError("Cannot capture image from camera")
+    
+    # Optionally, convert the image from BGR to RGB format
+    # frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    # Convert the captured frame (image) to a JPEG in memory
+    success, encoded_image = cv2.imencode('.jpg', frame)  # You can use frame_rgb if you converted it
+    if not success:
+        raise ValueError("Could not encode image to JPEG")
+    
+    # Convert the JPEG buffer to a Base64 string
+    base64_image_str = base64.b64encode(encoded_image).decode('utf-8')
+    
+    # Release the camera
+    cap.release()
+    
+    return base64_image_str
+
+def capture_image_to_base64():
+    # Initialize Picamera2
+    picamera2 = Picamera2()
+
+    # Configure the camera
+    config = picamera2.create_preview_configuration()
+    picamera2.configure(config)
+
+    # Start the camera and let it adjust to conditions
+    picamera2.start()
+
+    # Capture an image to a NumPy array
+    image = picamera2.capture_array()
+
+    # Convert the image from BGR to RGB format
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # This is the key step
+
+    # Convert the NumPy array to a JPEG in memory
+    success, encoded_image = cv2.imencode('.jpg', image_rgb)  # Use the RGB image here
+    if not success:
+        raise ValueError("Could not encode image")
+
+    # Convert the JPEG buffer to a Base64 string
+    base64_image_str = base64.b64encode(encoded_image.tobytes()).decode('utf-8')
+
+    # Stop the camera
+    picamera2.stop()
+
+    return base64_image_str
+
+
+def get_levels(data, long_term_noise_level, current_noise_level):
+    pegel = np.abs(np.frombuffer(data, dtype=np.int16)).mean()
+    long_term_noise_level = long_term_noise_level * 0.995 + pegel * (1.0 - 0.995)
+    current_noise_level = current_noise_level * 0.920 + pegel * (1.0 - 0.920)
+    return pegel, long_term_noise_level, current_noise_level
+
+
+def combine_bytes_to_base64(audio, audio_data):
+    # Create a bytes buffer to hold the WAV file data
+    wav_buffer = io.BytesIO()
+
+    # Initialize a wave file writer
+    with wave.open(wav_buffer, 'wb') as wav_file:
+        wav_file.setnchannels(1)  # Mono
+        wav_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))  # Sample width in bytes
+        wav_file.setframerate(44100)  # Sample rate
+        wav_file.writeframes(audio_data)  # Write the raw audio data
+
+    # Get the WAV file data from the buffer
+    wav_data = wav_buffer.getvalue()
+    
+    with open("audio.wav", "wb") as f:
+        f.write(wav_data)
+
+    # Encode the WAV file data to Base64
+    audio_data_base64 = base64.b64encode(wav_data).decode("utf-8")
+    return audio_data_base64
--- firmware/ver1/main.py
+++ firmware/ver1/main.py
@@ -1,199 +1,122 @@
-from dotenv import load_dotenv
 import numpy as np
 import shutil
 import base64
 import pyaudio
 import collections
-from websockets import connect
+import websockets
 import asyncio
 import json
 import cv2
 import queue
 import threading
-from picamera2 import Picamera2
+import io
+import wave
+# from picamera2 import Picamera2
 
 # mpv
 import shutil
 import subprocess
 
-load_dotenv()
+from helper import play_audio_stream, capture_image_to_base64_opencv, combine_bytes_to_base64, get_levels
 
 uri = "wss://api.mybeacon.tech/ws?client_id=1234"
-
-def is_installed(lib_name):
-    lib = shutil.which(lib_name)
-    if lib is None:
-        return False
-    return True 
-
-def play_audio_stream(audio_queue):
-    """
-    Function to be run in a separate thread for playing audio chunks.
-    It takes a queue from which it will continuously read and play audio chunks.
-    """
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16,  # 16-bit PCM format
-                    channels=1,  # mono audio
-                    rate=44100,  # Adjusted to 44.1kHz sample rate for pcm_44100
-                    output=True)
-
-    try:
-        while True:
-            chunk = audio_queue.get()  # Block until an item is available
-            if chunk is None:
-                break  # None is the signal to stop
-            data = base64.b64decode(chunk)
-            stream.write(data)
-    finally:
-        # Ensure resources are released even if there's an error
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
-
-def capture_image_to_base64():
-    # Initialize Picamera2
-    picamera2 = Picamera2()
-
-    # Configure the camera
-    config = picamera2.create_preview_configuration()
-    picamera2.configure(config)
-
-    # Start the camera and let it adjust to conditions
-    picamera2.start()
-
-    # Capture an image to a NumPy array
-    image = picamera2.capture_array()
-
-    # Convert the image from BGR to RGB format
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # This is the key step
-
-    # Convert the NumPy array to a JPEG in memory
-    success, encoded_image = cv2.imencode('.jpg', image_rgb)  # Use the RGB image here
-    if not success:
-        raise ValueError("Could not encode image")
-
-    # Convert the JPEG buffer to a Base64 string
-    base64_image_str = base64.b64encode(encoded_image.tobytes()).decode('utf-8')
-
-    # Stop the camera
-    picamera2.stop()
-
-    return base64_image_str
-
-
-def get_levels(data, long_term_noise_level, current_noise_level):
-    pegel = np.abs(np.frombuffer(data, dtype=np.int16)).mean()
-    long_term_noise_level = long_term_noise_level * 0.995 + pegel * (1.0 - 0.995)
-    current_noise_level = current_noise_level * 0.920 + pegel * (1.0 - 0.920)
-    return pegel, long_term_noise_level, current_noise_level
-
-async def stream_output(audio_stream):
-    if not is_installed("mpv"):
-        message = (
-            "mpv not found, necessary to stream audio. "
-            "On mac you can install it with 'brew install mpv'. "
-            "On linux and windows you can install it from https://mpv.io/"
-        )
-        raise ValueError(message)
-
-    mpv_command = ["mpv", "--no-cache", "--no-terminal", "--", "fd://0"]
-    mpv_process = subprocess.Popen(
-        mpv_command,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL,
-    )
-
-    audio = b""
-
-    async for chunk in audio_stream:
-        if chunk is not None:
-            mpv_process.stdin.write(chunk)  # type: ignore
-            mpv_process.stdin.flush()  # type: ignore
-            audio += chunk
-
-    if mpv_process.stdin:
-        mpv_process.stdin.close()
-    mpv_process.wait()
-
-    return audio
 
 async def main():
     audio_queue = queue.Queue()  # Using queue.Queue for thread-safe operations
     audio_thread = threading.Thread(target=play_audio_stream, args=(audio_queue,))
     audio_thread.start()
-    with connect(uri) as websocket:
-        while True:
-            image_base64 = None
-            audio = pyaudio.PyAudio()
-            stream = audio.open(
-                rate=16000,
-                format=pyaudio.paInt16,
-                channels=1,
-                input=True,
-                frames_per_buffer=512,
-            )
-            audio_buffer = collections.deque(maxlen=int((16000 // 512) * 0.5))
-            frames, long_term_noise_level, current_noise_level, voice_activity_detected = (
-                [],
-                0.0,
-                0.0,
-                False,
-            )
-            print("\n\nStart speaking. ", end="", flush=True)
+    while True:
+        try:
+            async with websockets.connect(uri) as websocket:
+                print("Connected to server")
+                while True:
+                    image_base64 = None
+                    audio = pyaudio.PyAudio()
+                    stream = audio.open(
+                        rate=44100,
+                        format=pyaudio.paInt16,
+                        channels=1,
+                        input=True,
+                        frames_per_buffer=1024,
+                    )
+                    audio_buffer = collections.deque(maxlen=int((16000 // 1024) * 0.5))
+                    frames, long_term_noise_level, current_noise_level, voice_activity_detected = (
+                        [],
+                        0.0,
+                        0.0,
+                        False,
+                    )
+                    print("\n\nStart speaking. ", end="", flush=True)
+                    while True:
+                        data = stream.read(1024)
+                        pegel, long_term_noise_level, current_noise_level = get_levels(
+                            data, long_term_noise_level, current_noise_level
+                        )
+                        audio_buffer.append(data)
 
-            while True:
-                image_base64 = capture_image_to_base64()
-                data = stream.read(512)
-                pegel, long_term_noise_level, current_noise_level = get_levels(
-                    data, long_term_noise_level, current_noise_level
-                )
-                audio_buffer.append(data)
+                        if (
+                            not voice_activity_detected
+                            and current_noise_level > long_term_noise_level + 300
+                        ):
+                            print("Listening.\n")
+                            # Save image to file
+                            
+                            voice_activity_detected = True
+                            ambient_noise_level = long_term_noise_level
+                            frames.extend(list(audio_buffer))
+                            
+                        if voice_activity_detected:
+                            frames.append(data)
+                            if current_noise_level < ambient_noise_level + 100:
+                                print("Stopped speaking.\n")
+                                image_base64 = capture_image_to_base64_opencv()
+                                with open("image.jpg", "wb") as f:
+                                    f.write(base64.b64decode(image_base64))
+                                break  # voice activity ends
 
-                if (
-                    not voice_activity_detected
-                    and current_noise_level > long_term_noise_level + 300
-                ):
-                    voice_activity_detected = True
-                    
-                    print("Listening.\n")
-                    ambient_noise_level = long_term_noise_level
-                    frames.extend(list(audio_buffer))
-
-                if voice_activity_detected:
-                    frames.append(data)
-                    if current_noise_level < ambient_noise_level + 100:
-                        break  # voice activity ends
-
-            stream.stop_stream(), stream.close(), audio.terminate()
-            audio_data = b"".join(frames)
-            audio_data_base64 = base64.b64encode(audio_data).decode("utf-8")
-            
-            jsonBody = {
-                'audio': audio_data_base64,
-                'event': 'video_chat',
-                'mode': 'text',
-                'image': image_base64
-            }
-            await websocket.send(json.dumps(jsonBody))
-            while True:
-                try:
-                    response = await websocket.recv()
-                    response_data = json.loads(response)
-                    if response_data.get('transcript'):
-                        print(response_data.get('transcript'))
-                    if response_data.get('text'):
-                        print(response_data.get('text'), end="")
-                    if response_data.get('audio'):
-                        audio_chunk = response_data.get('audio')
-                        audio_queue.put(audio_chunk)
-                    if response_data.get('end'):
-                        audio_buffer.put(None)
-                        break
-                except Exception as e:
-                    print(e)
-                    await asyncio.sleep(1)
+                        
+                    stream.stop_stream(), stream.close(), audio.terminate()
+                    audio_data = b"".join(frames)
+                    audio_data_base64 = combine_bytes_to_base64(audio, audio_data)
+                                      
+                    jsonBody = {
+                        'audio': audio_data_base64,
+                        'event': 'visual_chat',
+                        'mode': 'text',
+                        'image': image_base64,
+                        'segment_data': ""
+                    }
+                    await websocket.send(json.dumps(jsonBody))
+                    while True:
+                        try:
+                            response = await websocket.recv()
+                            response_data = json.loads(response)
+                            if response_data.get('transcript'):
+                                print(response_data.get('transcript'))
+                            if response_data.get('text'):
+                                print(response_data.get('text'), end="")
+                            if response_data.get('audio'):
+                                audio_chunk = response_data.get('audio')
+                                # Convert from base64 to audio_data
+                                audio_queue.put(audio_chunk)
+                            if response_data.get('end'):
+                                audio_queue.put(None)
+                                # Save the audio to a file
+                                break
+                            if response_data.get('error'):
+                                print(response_data.get('error'))
+                                break
+                        except Exception as e:
+                            print(e)
+                            break
+        except websockets.exceptions.ConnectionClosed:
+            print("Connection closed")
+            await asyncio.sleep(5)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            break
+    print("Exiting...")
+    return ""
 
 import asyncio
-
 asyncio.run(main())
