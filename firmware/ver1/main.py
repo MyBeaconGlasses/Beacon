c4ff07b91c391b028efb40069b608b02c083a2ab
eyang-zeta <eyang.zeta@gmail.com>
2024-03-05 15:09:51 -0800

    audio test

--- firmware/ver1/main.py
+++ firmware/ver1/main.py
@@ -63,7 +63,7 @@ async def main():
                             
                         if voice_activity_detected:
                             frames.append(data)
-                            if current_noise_level < ambient_noise_level + 1000:
+                            if current_noise_level < ambient_noise_level + 100:
                                 print("Stopped speaking.\n")
                                 image_base64 = capture_image_to_base64()
                                 break  # voice activity ends
