5d80c116be4d8542655c6b73664bf053513a6929
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2024-04-18 11:11:01 -0700

    Adding demo code

--- website/app/layout.jsx
+++ website/app/layout.jsx
@@ -1,17 +1,23 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import {Inter} from 'next/font/google'
+import './globals.css'
+import Nav from '../components/Nav'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-  title: "Beacon",
-  description: "The OS for Everything Around You",
-};
+  title: 'Beacon',
+  description: 'The OS for Everything Around You',
+}
 
-export default function RootLayout({ children }) {
+export default function RootLayout({children}) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#070707]`}>{children}</body>
+      <body className={`${inter.className} bg-[#070707]`}>
+        <div className="relative">
+          <Nav />
+        </div>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
--- website/components/Nav.jsx
+++ website/components/Nav.jsx
@@ -1,24 +1,29 @@
-import './navbar.css';
+import './navbar.css'
 const Nav = () => {
   return (
     <div className="navbar">
-      <a href="#home" className="navbar-button">
+      <a href="/#home" className="navbar-button">
         Home
       </a>
-      <a href="#about" className="navbar-button">
+      <a href="/#about" className="navbar-button">
         About
-        </a>
-      <a href="#solution" className="navbar-button">
+      </a>
+      <a href="/#solution" className="navbar-button">
         Solution
-        </a>
-      <a href="#features" className="navbar-button">
+      </a>
+      <a href="/#features" className="navbar-button">
         Features
       </a>
-      <a href="#contact" className="navbar-button">
-        Contact
-        </a>
+      <div className="navbar-dropdown">
+        <button className="navbar-dropdown-button">Demos</button>
+        <div className="navbar-dropdown-content">
+          <a href="/demo">Segmentation Demo</a>
+          <a href="/voice_demo">Voice Demo</a>
+          <a href="/image_demo">Visual Demo</a>
+        </div>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Nav;
+export default Nav
